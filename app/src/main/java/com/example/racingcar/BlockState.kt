d4ef4a21c6c4d4bca65d06c0be7a45b4b2306d07
Behnam Banaei <behban20@gmail.com>
2023-07-14 12:47:59 +0330

    add blockers setup

---
+++ app/src/main/java/com/example/racingcar/BlockState.kt
@@ -0,0 +1,50 @@
+package com.example.racingcar
+
+import android.util.Log
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import com.example.racingcar.Constants.LANE_COUNT
+import com.example.racingcar.Constants.STREET_SIDE_PERCENTAGE_EACH
+
+data class BlockState(val image: ImageBitmap) {
+    private var blockers = mutableListOf<Blocker>(
+        Blocker(0, 0),
+        Blocker(1, 1),
+        Blocker(2, 2),
+    )
+
+    private var currentPosY = 0
+
+    fun move(velocity: Int) {
+        currentPosY += velocity
+    }
+
+    fun draw(drawScope: DrawScope) {
+        Log.d("mamad", "blocker y: $currentPosY")
+        drawScope.apply {
+            //todo change based on size.width
+            val blockerSize = 216
+            val initialOffsetX = (size.width.toInt() * STREET_SIDE_PERCENTAGE_EACH / 100)
+            val laneSize =
+                (size.width.toInt() * (100 - (2 * STREET_SIDE_PERCENTAGE_EACH)) / 100) / LANE_COUNT
+
+            blockers.forEachIndexed { index, blocker ->
+                val blockerOffsetX =
+                    initialOffsetX +
+                            ((laneSize - blockerSize) / 2) +
+                            (laneSize * blocker.lanePosition)
+                drawImage(
+                    image = image,
+                    srcOffset = IntOffset(0, 0),
+                    dstOffset = IntOffset(
+                        x = blockerOffsetX,
+                        y = currentPosY - (index * 400)
+                    ),
+//                    srcSize = IntSize(blockerSize, blockerSize)
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/racingcar/Blocker.kt
@@ -0,0 +1,3 @@
+package com.example.racingcar
+
+data class Blocker(val id: Int, val lanePosition: Int)
