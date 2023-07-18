50fe044cc19b63364eedb0b4062c712bf9e60790
Behnam Banaei <behban20@gmail.com>
2023-07-18 09:42:30 +0330

    add random positioning on blocker position reset“

--- app/src/main/java/com/example/racingcar/state/BlockState.kt
+++ app/src/main/java/com/example/racingcar/state/BlockState.kt
@@ -9,52 +9,47 @@ import com.example.racingcar.Constants.BLOCKER_INTERSPACE_PERCENTAGE
 import com.example.racingcar.Constants.BLOCKER_WIDTH
 import com.example.racingcar.Constants.LANE_COUNT
 import com.example.racingcar.Constants.STREET_SIDE_PERCENTAGE_EACH
-import com.example.racingcar.models.Blocker
+import kotlin.random.Random
 
-data class BlockState(val image: ImageBitmap) {
-    private val repetitionCount = 5 //todo magic numbers
-    private val blockers = List(repetitionCount) {//todo fix multiple blocker in same position
-        generateRandomLanesSegment()
-    }.flatten().toMutableList()
-
-    private fun generateRandomLanesSegment() =
-        (0 until LANE_COUNT).shuffled().map {
-            Blocker(id = System.currentTimeMillis(), lanePosition = it)
-        }
-
+data class BlockState(val image: ImageBitmap, var lanePosition: Int) {
     private var currentPosY = 0
 
     fun move(velocity: Int) {
         currentPosY += velocity
     }
 
-    fun draw(drawScope: DrawScope) {
+    fun draw(drawScope: DrawScope, index: Int) {
         drawScope.apply {
             //todo change based on size.width
             val initialOffsetX = (size.width.toInt() * STREET_SIDE_PERCENTAGE_EACH / 100)
             val laneSize =
                 (size.width.toInt() * (100 - (2 * STREET_SIDE_PERCENTAGE_EACH)) / 100) / LANE_COUNT
 
-            blockers.forEachIndexed { index, blocker ->
-                val blockerOffsetX =
-                    initialOffsetX +
-                            ((laneSize - BLOCKER_WIDTH) / 2) +
-                            (laneSize * blocker.lanePosition)
+            val blockerOffsetX =
+                initialOffsetX +
+                        ((laneSize - BLOCKER_WIDTH) / 2) +
+                        (laneSize * lanePosition)
 
-                val blockerOffsetY =
-                    currentPosY - (index * (size.height * BLOCKER_INTERSPACE_PERCENTAGE / 100).toInt())
+            val indexOffset = (index * (size.height * BLOCKER_INTERSPACE_PERCENTAGE / 100).toInt())
+            val blockerOffsetY = currentPosY - indexOffset
 
-                if (blockerOffsetY < size.height)
-                    drawImage(
-                        image = image,
-                        srcOffset = IntOffset(0, 0),
-                        dstOffset = IntOffset(
-                            x = blockerOffsetX,
-                            y = blockerOffsetY
-                        ),
-                        dstSize = IntSize(width = BLOCKER_WIDTH, height = BLOCKER_HEIGHT)
-                    )
+
+            if (currentPosY > size.height + indexOffset) {
+                currentPosY = indexOffset
+                //todo change position to avoid putting 2 consecutive blockers in same position
+                lanePosition = Random.nextInt(from = 0, until = LANE_COUNT)
             }
+
+            if (blockerOffsetY < size.height)
+                drawImage(
+                    image = image,
+                    srcOffset = IntOffset(0, 0),
+                    dstOffset = IntOffset(
+                        x = blockerOffsetX,
+                        y = blockerOffsetY
+                    ),
+                    dstSize = IntSize(width = BLOCKER_WIDTH, height = BLOCKER_HEIGHT)
+                )
         }
     }
 }
