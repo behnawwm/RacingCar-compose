7791ff56d8d51b7c62686442f3383b9160d187c3
Behnam Banaei <behban20@gmail.com>
2023-09-16 12:24:22 +0330

    enhance detectCarPositionByPointerInput

--- app/src/main/java/com/example/racingcar/ui/game/detectCarPositionByPointerInput.kt
+++ app/src/main/java/com/example/racingcar/ui/game/detectCarPositionByPointerInput.kt
@@ -2,7 +2,6 @@ package com.example.racingcar.ui.game
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.RequestDisallowInterceptTouchEvent
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import com.example.racingcar.models.CarPosition
 import com.example.racingcar.utils.Constants
@@ -11,10 +10,8 @@ import com.example.racingcar.utils.Constants
 fun Modifier.detectCarPositionByPointerInput(
     maxWidth: Int,
     onDetectPosition: (CarPosition) -> Unit
-) = Modifier.then(pointerInteropFilter(
-    requestDisallowInterceptTouchEvent = RequestDisallowInterceptTouchEvent()
-) {
-    val currentX = it.x
+) = Modifier.then(pointerInteropFilter { motionEvent ->
+    val currentX = motionEvent.x
 
     val laneIndex = (currentX / maxWidth)
         .toInt()
